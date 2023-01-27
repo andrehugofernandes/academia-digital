0cf31e93aa8aa248b9a3d63d8779b39540884d76
andrehugofernandes <andrehugofernandes@gmail.com>
2023-01-27 11:59:03 -0300

    Atualização das classes Aluno.java, AvalizaoFisica.java e Matricula.java

--- src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
+++ src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
@@ -18,15 +18,12 @@ public class AvaliacaoFisica {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @ManyToOne
+  @ManyToOne(cascade = CascadeType.ALL)
   @JoinColumn(name = "aluno_id")
   private Aluno aluno;
-
   private LocalDateTime dataDaAvaliacao = LocalDateTime.now();
-
   @Column(name="peso_atual")
   private double peso;
-
   @Column(name="altura_atual")
   private double altura;
 
